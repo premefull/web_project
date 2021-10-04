6052feb98b905a18b6849edbb07efe919204aa75
premefull <43545924+premefull@users.noreply.github.com>
2021-10-04 23:06:53 +0700

    test3

--- page/login_admindb.php
+++ page/login_admindb.php
@@ -3,7 +3,7 @@
     include('connect.php');
     $errors =array();
 
-    if(isset($_POST['login_res'])){
+    if(isset($_POST['login_admin'])){
         $username = mysqli_real_escape_string($conn,$_POST['username']);
         $password = mysqli_real_escape_string($conn,$_POST['password']);
         
@@ -12,20 +12,21 @@
 
         if(count($errors)==0){
             
-            $query = "SELECT * FROM restaurant WHERE Res_nameID  = '$username' AND Password ='$password' ";
+            $query = "SELECT * FROM admin WHERE A_name  = '$username' AND A_password ='$password' ";
             $result = mysqli_query($conn , $query);
 
             if( mysqli_num_rows($result) == 1){
                
-                    $_SESSION['username'] =  $username ;
-                  
+                    $_SESSION['username'] = $username ;
                     $_SESSION['success'] = "Your are now logged in";
-                    header("location: store.php");
+                   
+                   
+                    header("location: page.html");
 
             }else{
                 array_push($errors,"Wrong username/password combination");
                 $_SESSION['error'] = "Wrong username or password or try again";
-                header("location: login_res.php");
+                header("location: login_admin.php");
             }
         }
     }
