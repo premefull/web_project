147ef1a2cd810d024a26430a9bb0b0a7e29624dc
premefull <43545924+premefull@users.noreply.github.com>
2021-10-06 09:41:19 +0700

    test4

--- page/signup_admin.php
+++ page/signup_admin.php
@@ -1,3 +1,7 @@
+<?php
+        session_start();
+        include('connect.php');
+?>
 <!DOCTYPE html>
 <!-- Coding By CodingNepal - youtube.com/codingnepal -->
 <html lang="en" dir="ltr">
@@ -14,37 +18,48 @@
         <h1>Signup Admin
             <Abbr></Abbr>
         </h1>
-        <form method="post">
+        <form method="post" action="signup_admindb.php">
+        <?php include('errors.php');?>
+        <?php if (isset($_SESSION['error'])) : ?>
+        <div class="error">
+            <h3>
+                <?php
+                    echo $_SESSION['error'];
+                    unset($_SESSION['error']);
+                ?>
+            </h3>
+        </div>
+        <?php endif ?>
             <div class="txt_field">
-                <input type="text" required>
-                <span></span>
-                <label>Username</label>
-            </div>
-            <div class="txt_field">
-                <input type="password" required>
-                <span></span>
-                <label>Password</label>
-            </div>
-            <div class="txt_field">
-                <input type="text" required>
+                <input type="Employee_id" name = "id_admin" required>
                 <span></span>
                 <label>Employee_id</label>
             </div>
             <div class="txt_field">
-                <input type="text" required>
+                <input type="username" name = "username_admin" required>
+                <span></span>
+                <label>Username</label>
+            </div>
+            <div class="txt_field">
+                <input type="surname" name = "surname_admin" required>
+                <span></span>
+                <label>Surname</label>
+            </div>
+            <div class="txt_field">
+                <input type="password" name ="password_admin" required>
+                <span></span>
+                <label>Password</label>
+            </div>
+            <div class="txt_field">
+                <input type="Email" name ="email_admin" required>
                 <span></span>
                 <label>Email</label>
             </div>
-            <div class="txt_field">
-                <input type="text" required>
-                <span></span>
-                <label>Tel.</label>
-            </div>
             <!--<div class="pass">Forgot Password?</div> -->
-            <input type="submit" value="Signup">
+            <button type="submit" name="signup_admin" herf="login_admin.php">login</button>
 
             <div class="signup_link">
-                <a href="login_admin.html">Back</a>
+                <a href="login_admin.php">Back</a>
             </div>
         </form>
     </div>
